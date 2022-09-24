3214d5612fb3aa83fbc10fb44252ab370dea87a1
VKlapan <vladimir.klapan@gmail.com>
2022-09-24 20:52:26 +0300

    add draft of load more feature

---
+++ src/components/ImageGallery/ButtonLoadMore/ButtonLoadMore.jsx
@@ -0,0 +1,9 @@
+const Button = clickHandle => {
+  return (
+    <button type="button" onClick={clickHandle}>
+      LOAD MORE
+    </button>
+  );
+};
+
+export default Button;
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,47 +3,58 @@ import axios from 'axios';
 
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 import { GalleryList } from './ImageGallery.styled';
+import ButtonLoadMore from './ButtonLoadMore/ButtonLoadMore';
 
 class ImageGallery extends Component {
   BASE_URL = 'https://pixabay.com/api/';
   API_KEY = '28999251-52156a0b70764a414979b8adf';
   PARAMS = 'image_type=photo&orientation=horizontal&safesearch=true';
   per_page = 12;
-  currentPage = 1;
 
   state = {
-    images: null,
+    images: [],
+    query: '',
     loading: false,
     error: null,
+    page: 1,
+    total: 1,
   };
 
-  async searchImage(query = '') {
-    this.setState({ loading: true, images: null, error: null });
-    await this.getPics(this.getUrl(query))
+  searchImage = query => {
+    this.setState({ loading: true, query: query, error: null, images: [] });
+  };
+
+  loadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  async getImages() {
+    await this.getGalleryData(this.getUrl())
       .then(result => {
-        this.setState({ images: result.images });
+        const newImages = [...this.state.images, ...result.images];
+        this.setState({ images: newImages, total: result.total });
       })
       .catch(error => this.setState({ error: error }))
       .finally(() => this.setState({ loading: false }));
   }
 
-  getUrl(query) {
-    const url = `${this.BASE_URL}?key=${this.API_KEY}&q=${query}&${this.PARAMS}&page=${this.currentPage}&per_page=${this.per_page}`;
+  getUrl() {
+    const url = `${this.BASE_URL}?key=${this.API_KEY}&q=${this.state.query}&${this.PARAMS}&page=${this.state.page}&per_page=${this.per_page}`;
     return url;
   }
 
-  async getPics(url) {
+  async getGalleryData(url) {
     const response = await axios.get(url);
-    const hitsArr = await response.data.hits;
+    const images = await response.data.hits;
     let result = null;
 
-    if (hitsArr.length === 0) {
+    if (images.length === 0) {
       return Promise.reject(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     } else {
       result = {
-        images: hitsArr,
+        images: images,
         total: response.data.totalHits,
       };
       return result;
@@ -51,35 +62,42 @@ class ImageGallery extends Component {
   }
 
   async componentDidMount() {
-    // await this.getPics(this.getUrl(this.props.query)).then(result =>
-    //   this.setState({ images: result.images })
-    // );
-    this.searchImage(this.props.query);
+    console.log('mount');
+    this.getImages();
   }
 
   async componentDidUpdate(prevProps, prevState) {
     if (prevProps.query !== this.props.query) {
-      // await this.getPics(this.getUrl(this.props.query)).then(result =>
-      //   this.setState({ images: result.images })
-      // );
       this.searchImage(this.props.query);
+    }
+
+    if (prevState.query !== this.state.query) {
+      this.getImages();
+    }
+
+    if (prevState.page !== this.state.page) {
+      this.getImages();
     }
   }
 
   render() {
-    const { images } = this.state;
-    //    if (!images) return;
+    const { images, total } = this.state;
+    const totalPages = Math.ceil(total / this.per_page);
+    console.log(totalPages);
 
     return (
       <>
         {this.state.loading && <div> LOADING..............</div>}
         {this.state.error && <div>Opsss... {this.state.error}</div>}
-        {images && (
-          <GalleryList>
-            {images.map(image => (
-              <ImageGalleryItem key={image.id} image={image} />
-            ))}
-          </GalleryList>
+        <GalleryList>
+          {images.map(image => (
+            <ImageGalleryItem key={image.id} image={image} />
+          ))}
+        </GalleryList>
+        {this.state.page < totalPages && (
+          <button type="button" onClick={this.loadMore}>
+            LOAD MORE
+          </button>
         )}
       </>
     );
